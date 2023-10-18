e92e79fc44d8d759ed2409a629f06b5ce07d0ce9
KLociJS <kisznerlorant21@gmail.com>
2023-10-18 14:30:37 +0200

    Add a little bit styling

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -1,32 +1,42 @@
 import React from "react";
 import useMapContext from "../../Context/useMapContext";
-import useGetDirection from "../../Hooks/useGetDirection";
 import "./SideBar.css";
 
 export default function SideBar() {
-  const { markers, setCoordinates, color, setColor, lineWidth, setLineWidth } =
+  const { markers, color, setColor, lineWidth, setLineWidth, routeData } =
     useMapContext();
-  const { getDirectionsFetch, routeData } = useGetDirection();
+
   return (
     <div className='sidebar-container'>
-      <button onClick={() => getDirectionsFetch(markers, setCoordinates)}>
-        Show route
-      </button>
-      <input
-        type='color'
-        value={color}
-        onChange={(e) => setColor(e.target.value)}
-      />
-      <input
-        type='range'
-        min={5}
-        max={15}
-        step={1}
-        value={lineWidth}
-        onChange={(e) => setLineWidth(e.target.value)}
-      />
-      {routeData.distance ? <p>Distance: {routeData.distance}</p> : null}
-      {routeData.duration ? <p>Duration: {routeData.duration}</p> : null}
+      {markers.map((m) => (
+        <input key={m.place_name} type='text' value={m.place_name} />
+      ))}
+      <div className='route-info'>
+        {routeData.distance ? (
+          <p>Distance: {Math.round(routeData.distance)}m</p>
+        ) : null}
+        {routeData.duration ? (
+          <p>Duration: {Math.round(routeData.duration)}s</p>
+        ) : null}
+      </div>
+      <div className='accessibility-container'>
+        <p>Edit Route appearance</p>
+        <input
+          type='color'
+          name='color-picker'
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+        <input
+          type='range'
+          name='route-line-width'
+          min={5}
+          max={15}
+          step={1}
+          value={lineWidth}
+          onChange={(e) => setLineWidth(e.target.value)}
+        />
+      </div>
     </div>
   );
 }
